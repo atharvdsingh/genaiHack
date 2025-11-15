5e5960f0420ef6c25c8afa2b1324101bd68eb6ef
ankitcoderzone <ankityadav559008@gmail.com>
2025-11-15 13:25:18 +0530

    files altered

--- EduTech/agent.py
+++ EduTech/agent.py
@@ -1,11 +1,60 @@
 #importing each and every important modules from google adk.
 from google.adk.agents import SequentialAgent
-from ..AgentToAgent import report_analyze_agent, weakness_detector_agent, quiz_generator_agent, frontend_quiz_agent
 
+from google.adk.agents import Agent
+from google.adk.tools import google_search
 
 """
                     TeacherAgent - Analyze marksheet, taking action on Weak Subjects and planning quizes for you on behalf of your weak subject
 """
+
+
+
+
+
+
+report_analyze_agent=Agent(
+    name = 'ReportAnalyzeAgent',
+    model='gemini-2.5-flash',
+    instruction="""You are a specilized report analyze agent. Your only job to use the google_search tool to determine weak_subject and strong_subject given by student and also if user providing you result in pdf or image format then apply same process of extracting weak_subject and strong_subject  """,
+    tools = [google_search],
+    output_key="report_analyze_finding",
+)
+
+
+weakness_detector_agent = Agent(
+    name='WeaknessDetector',
+    model='gemini-2.5-flash',
+    instruction="""Extract the one weak_subject from the report_analyze_finding: {report_analyze_finding}
+Create a concise tabular summary as a bulleted list with 3-5 key points.""",
+    output_key='weakness_finding_subject'
+
+)
+
+
+quiz_generator_agent=Agent(
+    name='QuizGeneratorAgent',
+    model='gemini-2.5-flash',
+    instruction="""give five quizes question on the provided weakness_finding_subject: {weakness_finding_subject}
+Create quiz in a concise tabular format""",
+    output_key='quiz_generate',
+
+)
+
+
+frontend_quiz_agent=Agent(
+    name='FrontendQuizAgent',
+    model='gemini-2.5-flash',
+    instruction="""Take the weakness_finding_subject: {weakness_finding_subject} and the quiz generated on the this weak subject and make both of them list of dictionaries as output for this agent
+
+""",
+    output_key='frontend_quiz_dict',
+
+
+)
+
+
+
 
 root_agent = SequentialAgent(
     name='TeacherAgent',
